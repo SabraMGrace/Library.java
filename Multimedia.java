42aa1d51a135fa778cb8d2343ec910cb4ee87749
SabraMGrace <sabramgrace@gmail.com>
2019-12-09 18:07:22 -0800

    Update Multimedia.java

--- Multimedia.java
+++ Multimedia.java
@@ -2,9 +2,20 @@ package library;
 
 public class Multimedia extends Library {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	public void CD() {
+		System.out.println("");
+	}
+
+	public void showItem() {
+		return itemName;
+	}
+	
+	public void showStatus() {
+		return status;
+	}
 
 	}
 
-}
+
+
