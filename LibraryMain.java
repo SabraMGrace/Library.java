b7f8dc4b059db8b1081eb3afd8cf2efac00ebd28
SabraMGrace <sabramgrace@gmail.com>
2019-12-09 18:04:26 -0800

    Update LibraryMain.java

--- LibraryMain.java
+++ LibraryMain.java
@@ -14,11 +14,38 @@ public abstract class Library {
 for (int i = 0; i < l.length; i++) {
 	
 	l[i].showItem();
-  l[i].showStatus();
-   System.out.println();
+	l[i].showStatus();
+	System.out.println();
 }
+System.out.println(" ")
 }
 
-
-
+	public static boolean showItem() {
+		String itemName
+		if itemName == true
+				return itemName;
+		System.out.println(itemName);
+	} else
+		return false;
+	System.out.println("The library does not have this item. Please email the help desk to suggest the title.")
 	}
+
+	public static boolean showStatus() {
+		int status{
+			1 == available;
+			2 == checked out;
+			3 == on hold, reserved;
+		}if status = 1 {
+				System.out.println("Your item is available!");
+		}else status = 2 {
+				System.out.println("The item is checked out. Submit a hold request.");
+		]else status = 3 {
+				System.out.println("This item is already on hold. You can submit another hold request.);")
+		}
+			
+		
+	}
+	}
+	}
+
+
